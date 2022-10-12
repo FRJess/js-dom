117b0a1ff9e6c3ad82b4c6b9fa853375e872ad04
FR_Jess <fr_jess@icloud.com>
2022-10-12 13:05:39 +0200

    add content

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log(document.querySelector(".red").innerHTML);
+
+console.log(document.querySelector("#lorem1").innerHTML);
+
+const elementLorem1 = document.querySelector("#lorem1");
+
+console.log(elementLorem1.classList);
+elementLorem1.classList.add("upper");
+
+elementLorem1.classList.remove("italic");
+
+console.log(elementLorem1.className);
+
+//seleziono il primo container
+const primoContainer = document.querySelector(".container");
+//lo nascondo aggiungendo la classe hide (commento per non applicarlo)
+// primoContainer.classList.add("hide");
+
+const terzoContainer = document.querySelector(".terzo")
+
+terzoContainer.style.cssText = "background-color: pink; font-size: 0.8rem; padding: 1rem; border: 1px dotted black";
+console.log("terzoContainer", terzoContainer.classList)
